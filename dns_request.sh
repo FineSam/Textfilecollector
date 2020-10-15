bd03085b04b4b5f9723492c3663958761648677b
FineSam <samherubel@gmail.com>
2020-10-15 14:07:12 +0900

    Update dns_request.sh

--- dns_request.sh
+++ dns_request.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+HOMEPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+FILES="$(ls $HOMEPATH | grep list)"
+
 while ((1)) ; do
-    python /home/prometheus/pushgateway/dnsrequesttime.py /home/prometheus/pushgateway/eu.global.list google.com
-    python /home/prometheus/pushgateway/dnsrequesttime.py /home/prometheus/pushgateway/jp.global.list google.com
-    sleep 1
+    for FILE in $FILES
+    do
+        python $HOMEPATH/dnsrequesttime.py $HOMEPATH/$FILE google.com
+        sleep 1
+    done
 done
